5025ce3364f1c7302bd9d5d1520490961814ed0b
gaoybBU <gaoyb@bu.edu>
2023-07-10 18:49:03 -0400

    Logging methods added

--- BasicEngine/src/BasicEngine.h
+++ BasicEngine/src/BasicEngine.h
@@ -3,3 +3,4 @@
 // Master header file, used by clients
 #include "Engine/Application.h"
 #include "Engine/EntryPoint.h"
+#include "Engine/Log.h"
--- BasicEngine/src/Engine/EntryPoint.h
+++ BasicEngine/src/Engine/EntryPoint.h
@@ -1,11 +1,19 @@
 #pragma once
 
+#ifdef ENG_PLATFORM_WINDOWS
+
 extern Engine::Application* Engine::CreateApplication(); 
 
-int main() {
 
+int main() {
+	Engine::Log::Init();
+	Engine::Log::GetClientLogger()->info("Client Info");
+	Engine::Log::GetCoreLogger()->trace("Core Trace");
+	
 	auto application = Engine::CreateApplication();
 	application->Run();
 	delete application;
 	return 0;
 }
+
+#endif
